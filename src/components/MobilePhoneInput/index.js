181a9470b225d9137da85d47f0ef6e9d6d93e62d
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-04-06 19:01:20 +0530

    [add] BASIC mobile footer code

--- src/components/MobilePhoneInput/index.js
+++ src/components/MobilePhoneInput/index.js
@@ -1,13 +1,61 @@
 import React from 'react'
 import { useMobile } from '../../MobileContext';
-import './index.css';
 import AgreeText from '../../constants/AgreeText';
 function MobilePhoneInput() {
   const {mobile}=useMobile();
   return mobile &&(
     <div
-    className='MobilePhoneInput'
+    style={{
+      position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 1000;
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 110%);
+    width: 100%;
+    display: flex ;
+    flex-direction: column;
+    }}
     >
+      <div style={{
+        width:'100%',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+      }}>
+        <div style={{
+          position:'relative',
+          top:'2vh',
+          width:"84vw",
+          background:"black",
+          padding:'1vh 1rem',
+          borderTopLeftRadius:'10px',
+          borderTopRightRadius:'10px',
+          borderColor:"rgb(253, 239, 120)",
+          borderWidth:'1px',
+          borderStyle:'solid',
+          borderBoxing:'border-box',
+          zIndex:'-1',
+          borderRadius:'10px',
+          paddingBottom:'1rem'
+        }}>
+      <input
+          type="text"
+          // value={phoneNumber}
+          maxLength="10"
+          placeholder="Enter phone number"
+          // onChange={(e) => setPhoneNumber(e.target.value)}
+          style={{
+            height: "35px",
+            fontSize: "15px",
+            outline: "none",
+            border: "none",
+            color: "white",
+            backgroundColor: "black",
+            width: "150px",
+            paddingLeft: "5px",
+          }}
+        />
+        </div>
       <div style={{
         display:'flex',
         justifyContent:"space-between",
@@ -21,15 +69,15 @@ function MobilePhoneInput() {
         Apply now
         <img src="https://www.uni.cards/images/right_arrow.svg"/>
       </div>
+      </div>
       <label
         style={{
           display: "flex",
           alignItems: "left",
           width: "300px",
           fontSize: "10px",
-          width:"80%",
           color:"white",
-          marginTop:"1rem",
+          margin:"1rem",
         }}
       >
         <input
