8b847f16830df035f4708c60bd31c870bc09b761
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-04-06 18:20:18 +0530

    [add] input box

--- src/components/MobilePhoneInput/index.js
+++ src/components/MobilePhoneInput/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useMobile } from '../../MobileContext';
 import './index.css';
+import AgreeText from '../../constants/AgreeText';
 function MobilePhoneInput() {
   const {mobile}=useMobile();
   return mobile &&(
@@ -10,16 +11,39 @@ function MobilePhoneInput() {
       <div style={{
         display:'flex',
         justifyContent:"space-between",
-        margin:'0 5vw',
         background:"rgb(253, 239, 120)",
         padding:'1rem',
         fontFamily:'"Matter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
         borderRadius:'10px',
         fontSize:'15px',
+        width:'85vw'
       }}>
         Apply now
         <img src="https://www.uni.cards/images/right_arrow.svg"/>
       </div>
+      <label
+        style={{
+          display: "flex",
+          alignItems: "left",
+          width: "300px",
+          fontSize: "10px",
+          width:"80%",
+          color:"white",
+          marginTop:"1rem",
+        }}
+      >
+        <input
+          type="checkbox"
+          // checked={isAgreed}
+          // onChange={(e) => setIsAgreed(e.target.checked)}
+          style={{ margin: "0 5px",
+          background: "transparent",
+          border:"none",
+        
+         }}
+        />
+        {AgreeText}
+      </label>
     </div>
   )
 }
