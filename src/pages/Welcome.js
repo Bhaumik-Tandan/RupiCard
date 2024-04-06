0d5f9655d1d963f1a025e0801776229726a28513
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-04-06 05:59:20 +0530

    [AD] videO background

---
+++ src/pages/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Video from '../components/Video';
+
+function Welcome() {
+  return (
+    <div style={{ height: "100vh", width: "100vw"}}>
+      ls
+      <Video />
+    </div>
+  )
+}
+
+export default Welcome;
